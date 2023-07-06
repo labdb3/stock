c4992055820c388e9206e5718d505823868bd7cf
gta0804 <573148536@qq.com>
2023-07-06 16:19:37 +0800

    initial

---
+++ stock_analysis/stock/get_alpha.py
@@ -0,0 +1,40 @@
+from Alpha_code_1 import *
+import pymysql
+import tushare as ts
+
+def get_stock(start_date, end_date, code):
+    # 连接数据库
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='root',
+                                 db='stock_analysis')
+
+    try:
+        # 创建游标对象
+        cursor = connection.cursor()
+
+        # 执行 SQL 查询
+        sql = "SELECT * FROM stock WHERE trade_date BETWEEN %s AND %s AND ts_code = %s order by trade_date asc"
+        cursor.execute(sql, (start_date, end_date, code))
+
+        # 获取查询结果
+        results = cursor.fetchall()
+
+        # 将结果转换为 DataFrame 格式
+        df = pd.DataFrame(results, columns=[desc[0] for desc in cursor.description])
+
+        return df
+
+    except Exception as e:
+        print("Error occurred while fetching data from database:", str(e))
+
+    finally:
+        # 关闭游标和数据库连接
+        cursor.close()
+        connection.close()
+
+
+demo_result = get_stock("2020-01-01","2023-07-05","430047.BJ")
+##print(demo_result1)
+another_alpha = get_alpha(demo_result)
+##print(demo_alpha.head(10))
---
+++ stock_analysis/stock/import_stock.py
@@ -0,0 +1,26 @@
+import pymysql
+import pandas as pd
+import csv
+import os
+
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='root',password='root', db='stock_analysis')
+c=conn.cursor()
+table = 'stock'
+header = ['ts_code','trade_date', 'open','high','low', 'close','pre_close','pct_chg','vol', 'amount']
+fpath = 'a_share.csv'
+file = open(fpath)
+cr = csv.reader(file, delimiter=',')
+next(cr)
+
+
+sqltemp = 'insert into {}({}) value({})'.format(table, ','.join(header), ('%s,'*len(header))[:-1])
+for row in cr:  
+    try:
+        row = [None if r == '' else r for r in row]
+        c.execute(sqltemp, row)
+        print("inserting "+ str(row))
+    except Exception as e:
+        print(row)
+        raise e
+conn.commit()
+file.close()
